9e6813688547495360f8c32ac0393e012d96a431
Jonathan <jsurgeon29@gmail.com>
2021-10-21 12:41:10 -0700

    created Day class- get, set, delete methods

---
+++ entry.py
@@ -0,0 +1,72 @@
+# file containing classes Day() and Entry() 
+
+from activity import *
+from datetime import date
+class Day:
+    """
+    Day is a base parent class
+    Attributes:
+        Rest sleep
+        Activity activities[]
+        Habit habits[]
+        Exercise workouts[]
+    """
+    def __init__(self, rest, acts, habs, works):
+        self._sleep = rest
+        self._activities = acts
+        self._habits = habs
+        self._workouts = works
+
+    # sleep attribute get, set, delete
+    @property
+    def sleep(self):
+        return self._sleep
+    
+    @sleep.setter
+    def sleep(self, rest):
+        self._sleep = rest
+    
+    @sleep.deleter
+    def sleep(self):
+        del self._sleep
+
+    # activities attribute get, set, delete
+    @property
+    def activities(self):
+        return self._activities
+    
+    @activities.setter
+    def activities(self, acts):
+        self._activities = acts
+    
+    @activities.deleter
+    def activities(self):
+        del self._activities
+    
+    # habits attribute get, set, delete
+    @property
+    def habits(self):
+        return self._habits
+    
+    @habits.setter
+    def habits(self, habs):
+        self._habits = habs
+    
+    @habits.deleter
+    def habits(self):
+        del self._habits
+
+    # workouts attribute get, set, delete
+    @property
+    def workouts(self):
+        return self._workouts
+    
+    @workouts.setter
+    def workouts(self, works):
+        self._workouts = works
+    
+    @workouts.deleter
+    def workouts(self):
+        del self._workouts
+
+        
